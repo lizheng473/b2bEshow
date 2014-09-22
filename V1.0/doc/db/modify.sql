c65975cbbb4307ac80b50f15c8fa30339f1afb62
lizheng473 <lizheng473@163.com>
2014-09-22 22:53:42 +0800

    数据结构修改

---
+++ V1.0/doc/db/modify.sql
@@ -0,0 +1,6 @@
+--20140921ÐÞ¸ÄProduct±í
+alter table Product add  material varchar(50);
+alter table Product add  origin varchar(50);
+alter table Product add   size varchar(50);
+alter table Product add   comcode  char(8) after userid ;
+alter table Product add   imgpath  varchar(100) after img ;
